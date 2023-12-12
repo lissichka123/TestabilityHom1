8fe6e0b0eb8bdf7494f10c6dac2b86e9d1ae8cc7
Ekaterina Turkina <katrina.turkina@gmail.com>
2023-12-12 10:07:22 +0300

    reformat code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,8 +1,8 @@
 public class BonusMilesService {
-public int calculate(int price){
-  int oneMile = 20;
-  int totalMiles;
-  totalMiles = price / oneMile;
-  return totalMiles;
-}
+    public int calculate(int price) {
+        int oneMile = 20;
+        int totalMiles;
+        totalMiles = price / oneMile;
+        return totalMiles;
+    }
 }
